b63c9c9ad0491cc11eca8c0d53c1d2058fc2ff8e
Nicolas Torre <10921378+nicolastorre@users.noreply.github.com>
2025-05-26 08:26:25 +0200

    [frontend] refacto: add BASE_URL api config

--- packages/frontend/src/hooks/usePosts.ts
+++ packages/frontend/src/hooks/usePosts.ts
@@ -1,14 +1,13 @@
 import { Post } from "domain/entities/Post";
 import type { IPost } from "domain/interfaces/entities/IPost";
 import { useCallback, useEffect, useState } from "react";
+import { BASE_URL } from "../config/apiConfig";
 
 export function usePosts() {
   const [posts, setPosts] = useState<IPost[]>([]);
 
   const getPosts = (): Promise<IPost[]> => {
-    const baseUrl = import.meta.env.VITE_BASE_URL;
-
-    return fetch(`${baseUrl}/posts`)
+    return fetch(`${BASE_URL}/posts`)
       .then((response) => response.json())
       .then((json) => json.map((data: IPost) => new Post(data)));
   };
